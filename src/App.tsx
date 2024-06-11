2ed96b9f4bdb91733a0ce7a1c62715d378dc0fb4
Dean Longstaff <dean@deanlongstaff.com>
2024-06-11 22:30:49 +0100

    Implement MS Clarity Data Collector

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Import the required modules
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { clarity } from "react-microsoft-clarity";
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Import the app components
 
-import ScrollToTop from "./components/ScrollToTop"
+import ScrollToTop from "./components/ScrollToTop";
 import Preloader from "./components/Preloader";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/index";
@@ -22,12 +23,17 @@ import Footer from "./components/Footer";
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Import the app styles
 
-import './styles/index.css';
+import "./styles/index.css";
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Define the App component
 
 function App() {
   const [load, upadateLoad] = useState(true);
+
+  // -- Initialise Microsoft Clarity
+  useEffect(() => {
+    clarity.init("mqgrxsm7hq");
+  }, []);
 
   // -- Set a timer to remove the preloader after 1.2 seconds
   useEffect(() => {
