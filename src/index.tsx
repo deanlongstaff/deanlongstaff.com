40d361f278370fc4616c1cf47d7fdc5dadcedac8
Dean Longstaff <dean@deanlongstaff.com>
2026-08-07 20:53:09 +0100

    image preload fix

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import photo from "./assets/images/me.jpeg";
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Import the app styles
 
 import "./styles/index.css";
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----- Preload the preloader's photo so it appears immediately instead of popping in after the Preloader mounts
+
+const preloadLink = document.createElement("link");
+preloadLink.rel = "preload";
+preloadLink.as = "image";
+preloadLink.href = photo;
+document.head.appendChild(preloadLink);
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----- Render the App component to the DOM
 
